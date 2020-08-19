8936a0578ec16cb81545b2d80f23b0c08eb35a0e
Nestor Fernandez <antoniazo@outlook.es>
2020-08-19 16:09:07 +0200

    versión del front builded

---
+++ frontend/build/precache-manifest.36dd563379ea08de5465f32143ef9ac7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b4daf216e1d1a2334ca86faa70187b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7861801e7bd9c1419c1",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "300a40581c13318f4028",
+    "url": "/static/css/main.a5ae5d4a.chunk.css"
+  },
+  {
+    "revision": "e7861801e7bd9c1419c1",
+    "url": "/static/js/2.3dcfffdd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3dcfffdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "300a40581c13318f4028",
+    "url": "/static/js/main.dbc5bd5d.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "68fcf38c8c8e178799686d15abef5944",
+    "url": "/static/media/bootstrap-logo.68fcf38c.png"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "/static/media/mysql-logo.f80a91ba.svg"
+  },
+  {
+    "revision": "26bdcd1522937d05915328db94148a7a",
+    "url": "/static/media/node-express-logo.26bdcd15.png"
+  },
+  {
+    "revision": "e4fc46831fda396a5392e97f0f79e76f",
+    "url": "/static/media/react-logo.e4fc4683.jpg"
+  },
+  {
+    "revision": "c87187cad0ab07cb31c76fb8facda1e1",
+    "url": "/static/media/typescript-logo.c87187ca.svg"
+  }
+]);
